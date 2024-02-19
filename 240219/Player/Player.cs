62b6c03435a66d558e0723ddbbe8970d9fe04520
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-02-19 14:20:11 +0900

    Add files via upload

---
+++ 240219/Player/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// 실습_240219
+    /// 1. 입력처리
+    ///     1.1. Player Input Action 맵 만들기
+    /// 2. Move 애니메이션 만들기
+    ///     2.1. 10 프레임 간격으로 0~3번
+    /// 3. Move Blend Tree 만들기
+}
