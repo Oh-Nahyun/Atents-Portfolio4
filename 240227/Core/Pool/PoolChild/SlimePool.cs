4196c21d7a2ffb9f492550274284b95ade52f173
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-02-27 15:02:20 +0900

    Add files via upload

---
+++ 240227/Core/Pool/PoolChild/SlimePool.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimePool : ObjectPool<Slime>
+{
+    protected override void OnGenerateObject(Slime comp)
+    {
+        comp.Pool = comp.transform.parent; // pool 설정
+    }
+}
