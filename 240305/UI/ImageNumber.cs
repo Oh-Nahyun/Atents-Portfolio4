84bfefecd570329c47246e7ffc471ad9e3b4137c
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-03-05 14:23:13 +0900

    Add files via upload

---
+++ 240305/UI/ImageNumber.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageNumber : MonoBehaviour
+{
+    public Sprite[] numberImages;
+
+    Image[] digits;
+
+    /// <summary>
+    /// 목표
+    /// </summary>
+    int number = 0;
+
+    public int Number
+    {
+        get => number;
+        set
+        {
+            if (number != value)
+            {
+                number = value;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        digits = GetComponentsInChildren<Image>();
+    }
+
+    private void Start()
+    {
+        //int a = Number % 10;
+        //int b = a % 10;
+        //int c = b % 10;
+        //int d = c % 10;
+        //int e = d % 10;
+
+        //if (e == 0)
+        //{
+        //    digits[4].disable();
+        //}
+
+        //for (int i = 0; i < digits.Length; i++)
+        //{
+        //    digits[i] = numberImages[Number % (10 ^ (i + 1))];
+        //}
+    }
+}
+
+/// 실습_240305
+/// number는 5번째 자리까지 표현 가능 (max = 99999)
+/// number에 값을 세팅하면 digits에 적절한 이미지가 선택된다.
+/// 사용되지 않는 자리는 disable 처리
---
+++ 240305/UI/LifeTimeText.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LifeTimeText : MonoBehaviour
+{
+    TextMeshProUGUI timeText;
+    float maxLifeTime;
+
+    private void Awake()
+    {
+        timeText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        Player player = GameManager.Instance.Player;
+        maxLifeTime = player.maxLifeTime;
+
+        player.onLifeTimeChange += OnLifeTimeChange;
+
+        timeText.text = $"{maxLifeTime:f2} Sec";
+    }
+
+    private void OnLifeTimeChange(float ratio)
+    {
+        timeText.text = $"{(maxLifeTime * ratio):f2} Sec";
+    }
+}
